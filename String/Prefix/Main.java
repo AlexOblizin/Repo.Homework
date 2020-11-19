e271c45eca63b586f2144069e4508c7a9c6ba9ac
AlexOblizin <shura.oblizin@mail.ru>
2020-11-19 10:05:48 +0300

    additions2

---
+++ String/Prefix/Main.java
@@ -0,0 +1,15 @@
+package HomeWorks.String.Prefix;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] arrayString = new String[]{"Привет", "Приветствую", "пфыр", "приват"};
+
+        Task.longestCommonPrefix(arrayString);
+    }
+
+
+
+
+
+}
